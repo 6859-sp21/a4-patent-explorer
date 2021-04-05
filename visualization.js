5f42f8103c12a46e580411f7d9276ae2b4f713b3
Eamon Whalen <ewhalen@altair.com>
2021-04-04 23:43:10 -0400

    added backgroud for interacting on ocean. removed antarctica

--- visualization.js
+++ visualization.js
@@ -1,10 +1,13 @@
-const width = 1800
-const height = 900
+// const width = 1800
+// const height = 900
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-const zoomMin = 0.5
-const zoomMax = 4
+const zoomMin = 1.0
+const zoomMax = 50
 
 const markerRadius = 3
+
 
 // create the svg canvas for our visualization
 const svg = d3.select('body')
@@ -12,20 +15,32 @@ const svg = d3.select('body')
               .attr('width', width)
               .attr('height', height);
 
-// create pan and zoom interaction
-const g = svg.append('g')
+
+// create map container with pan and zoom
+const map = svg.append('g')
+              .attr('width', width)
+              .attr('height', height)
               .call(d3.zoom()
-              .scaleExtent([zoomMin, zoomMax])
-              .on("zoom", zoomed));
+                .scaleExtent([zoomMin, zoomMax])
+                .extent([[0, 0], [width, height]])
+                .on("zoom", zoomed));
 
 function zoomed({transform}) {
-  g.attr("transform", transform);
-  g.selectAll("circle")
-    .attr("r", markerRadius/transform.k);
-
+  map.attr("transform", transform);
+  map.selectAll("circle")
+    .attr("r", markerRadius/transform.k); // keep marker size constant on screen
 }
 
-// create the base map
+// add background rectangle to enable interaction anywhere on map
+background = map.append('rect')
+  .attr('x', 0)
+  .attr('y', 0)
+  .attr('width', width)
+  .attr('height', height)
+  .attr('fill', 'gray');
+
+
+// add countries to map
 const projection = d3.geoMercator().scale(200)
   .translate([width/2, height/1.4]);
 const geoPath = d3.geoPath(projection);
@@ -33,26 +48,28 @@ const geoPath = d3.geoPath(projection);
 d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
   .then(data => {
     const countries = topojson.feature(data, data.objects.countries);
-    g.selectAll('path')
+    map.selectAll('path')
       .data(countries.features)
       .enter()
       .append('path')
+      .filter(function(d){ return d.properties.name !== "Antarctica" })
       .attr('class', 'country')
       .attr('cursor', 'pointer')
       .attr('d', geoPath);
 });
 
 
-// create point markers
+// add point markers to map
 d3.csv("Patent_5yrs_lat+long.csv")
   .then(function(data) {
     var latlonglist = [];
-    // for (var i=0; i < data.length/1000; i++) {
+    // for (var i=0; i < data.length; i++) {
     for (var i=0; i < 100; i++) {
       latlonglist.push([data[i].longitude,data[i].latitude]);
   }
-  g.selectAll("circle")
-  .data(latlonglist).enter()
+  map.selectAll("circle")
+  .data(latlonglist)
+  .enter()
   .append("circle")
   .attr("cx", function (d) { return projection(d)[0]; })
   .attr("cy", function (d) { return projection(d)[1]; })
@@ -85,28 +102,6 @@ d3.csv("Patent_5yrs_lat+long.csv")
 //     .attr("cx", function (d) { return projection(d)[0]; })
 //     .attr("cy", function (d) { return projection(d)[1]; });
 // }
-
-
-
-  // Load lat and long from csv file
-  // d3.csv("Patent_5yrs_lat+long.csv").then(function(data) {
-  //   var latlonglist = [];
-  //   // for (var i=0; i < data.length/1000; i++) {
-  //   for (var i=0; i < 100; i++) {
-  //     latlonglist.push([data[i].longitude,data[i].latitude]);
-  // }
-  // // svg.selectAll("circle")
-  // g.selectAll("circle")
-  // .data(latlonglist).enter()
-  // .append("circle")
-  // .attr("cx", function (d) { console.log(projection(d)); return projection(d)[0]; })
-  // .attr("cy", function (d) { return projection(d)[1]; })
-  // .attr("r", "2px")
-  // .attr("fill", "gold")
-  // .attr('cursor', 'pointer');
-  // });
-
-
 
 
   // function main() {
