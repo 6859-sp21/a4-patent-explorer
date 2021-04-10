42b05d5a6fd2554f3fbcc8edba962634cf2d86aa
bryanongwx <68488387+bryanongwx@users.noreply.github.com>
2021-04-10 18:59:27 -0400

    Year Slider
    Reformatted js file to include slider that filters according to year. Old code in "old code" folder for reference.

--- visualization.js
+++ visualization.js
@@ -86,114 +86,104 @@ d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
 });
 
 // add point markers to map
+// d3.csv("Patent_5yrs_all.csv")
+//   .then(function(data) {
+//     var latlonglist = [];
+//     // for (var i=0; i < data.length; i++) {
+//     for (var i=0; i < 1000; i++) {
+//       latlonglist.push([data[i].longitude, data[i].latitude, data[i].year, data[i].city,data[i].state,data[i].country,data[i].organization]);
+//   }
 d3.csv("Patent_5yrs_all.csv")
   .then(function(data) {
-    var latlonglist = [];
-    // for (var i=0; i < data.length; i++) {
-    for (var i=0; i < 1000; i++) {
-      latlonglist.push([data[i].longitude, data[i].latitude, data[i].year, data[i].city,data[i].state,data[i].country,data[i].organization]);
-  }
-  selection = [];
-  // const symbol = d3.symbol();
-  map.selectAll("circle")
-  .data(latlonglist)
-  .enter()
-  .append("circle")
-  //.filter(function(d){ return d[6] === selection})
-  //.filter(function(d){ return d[2] === "2018"})
-  .attr("cx", function (d) { return projection(d)[0]; })
-  .attr("cy", function (d) { return projection(d)[1]; })
-  .attr("r", markerRadius)
-  .attr("fill", "gold")
-  .attr('class', 'marker')
-  .attr('cursor', 'pointer')
-  .style('opacity', 0.6)
-
-  // d3.select("#range1").on("input", function(){
-  //   map.selectAll('circle')
-  //   .data(latlonglist)
-  //   .enter()
-  //   .append("circle")
-  //   .filter(function(d){
-  //     return d[2] === d3.select("#range1").property("value")})
-  //   .attr("r", markerR  adius)
-  //   .attr("fill", "gold")
-  //   .attr('class', 'marker')
-  //   .attr('cursor', 'pointer')
-  // })
-
-
-  .on("mouseover", function(d, i) {
-    tooltip.transition()
-      .duration(500) //animation technique makes the tooltips visible
-      .style("opacity", .9);
-    tooltip.html("Location: " + (d)[3] +" "+(d)[4] +", "+(d)[5] + "<br/> Company: " +(d)[6])
-      .style("left", (d3.event.pageX) + "px")
-      .style("top", (d3.event.pageY - 28) + "px");
-      //console.log( "Longitude " + (d)[0] + "<br/> Latitude: " +(d)[1])
-
-     d3.select(this) //select the point and change its properties on mouseover
-       .transition()
-       .duration(300)
-       .attr("fill", "white")
-       .style("opacity", 1)
-       // .attr("r", (5 * markerRadius/transform.k))
+    var id = 0;
+    data.forEach(function(d) {
+      d.id = id;
+      id = id + 1;
     })
 
-  .on("mouseout", function(event, d) {
-    tooltip.transition()
-      .duration(300)
-      .style("opacity", 0);
+    var displayCircles = function(d) {
+      var circles = map.selectAll("circle")
+                    .data(d.slice(0,2000), function(d) { return d.id })
+                    .enter()
+                    .append("circle")
+                      .attr("cx", function (d) { return projection([d.longitude, d.latitude])[0] })
+                      .attr("cy", function (d) { return projection([d.longitude, d.latitude])[1] })
+                      .attr("r", markerRadius)
+                      .attr("fill", "gold")
+                      .attr('class', 'marker')
+                      .attr('cursor', 'pointer')
+                    .style('opacity', 0.6);
 
-    d3.select(this)
-      .transition()
-      .attr("r", markerRadius)
-      .style("opacity", 0.6)
-      // .attr("r", markerRadius/transform.k)
+        circles.transition()
+                .delay(400)
+                .duration(400);
 
-    if(isClicked === false){
-      d3.select(this) //restore point to original value
-         .transition()
-         .duration(300)
-         .attr("fill", "gold")
-         //.attr("r", markerRadius)
-         .attr("r", markerRadius)
-         // .attr("r", markerRadius/transform.k)
+        circles.on("mouseover", function(d, i) {
+              tooltip.transition()
+                .duration(500) //animation technique makes the tooltips visible
+                .style("opacity", .9);
+              tooltip.html("Location: " + d.city +" "+ d.state +", "+ d.country + "<br/> Company: " + d.organization)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+                //console.log( "Longitude " + (d)[0] + "<br/> Latitude: " +(d)[1])
+
+               d3.select(this) //select the point and change its properties on mouseover
+                 .transition()
+                 .duration(300)
+                 .attr("fill", "white")
+                 .style("opacity", 1)
+                 // .attr("r", (5 * markerRadius/transform.k))
+              });
+
+        circles.on("mouseout", function(event, d) {
+          tooltip.transition()
+            .duration(300)
+            .style("opacity", 0);
+
+          d3.select(this)
+            .transition()
+            .attr("r", markerRadius)
+            .style("opacity", 0.6)
+            // .attr("r", markerRadius/transform.k)
+
+          if(isClicked === false){
+            d3.select(this) //restore point to original value
+               .transition()
+               .duration(300)
+               .attr("fill", "gold")
+               //.attr("r", markerRadius)
+               .attr("r", markerRadius)
+               // .attr("r", markerRadius/transform.k)
+          }
+        });
+
+        };
+
+    var removeCircles = function(d) {
+        map.selectAll("circle")
+            .data(d, function(d) { return d.id })
+            .exit()
+            .transition()
+              .duration(400)
+              .attr("r", 1)
+            .remove();
     }
-  })
 
-  // highlight related companies
+    displayCircles(data.filter( function(d) { return +d.year === 2015 }));
 
-  .on("click", function(d,i){
-    isClicked = true;
-    selection = d[2];
-    d3.selectAll(d[2]==="2018").style("fill-opacity", 1)
-    .attr("r", 5*markerRadius);
-    d3.select(this)
-    .transition()
-    .attr('fill',"white")
-    .attr("r", markerRadius/transform.k)
-    .style('opacity', 1)
-    //console.log(selection)
-  });
+    d3.select("#range1").on("change", function() {
+        var selectedValue = this.value;
+        var newData = data.filter( function(d) { return +d.year === +selectedValue });
+        displayCircles(newData);
+        removeCircles(newData);
+        }
+      );
 
-console.log(selection)
 
-  });
+});
 
-// d3.select("#range1").on("input", function(){
-//
-//   svg.selectAll('circle')
-//   .transition()
-//   .attr("fill", "gold")
-//   .filter(function(d){
-//
-//     return d[2] === d3.select("#range1").property("value")})
-//   // .attr("fill", "red")
-// });
-// add text
 
-var showText = true
+var showText = true;
 title = svg.append('text')
   .text('Patent Explorer')
   .attr('x', 30)
@@ -211,6 +201,13 @@ description.append('tspan')
     .text('Explore patents filed around the world:')
     .attr('dy', 30)
     .attr('x', 30)
+description.append('tspan')
+    .text('start')
+    .attr('dy', 30)
+    .attr('x', 50)
+    .attr('class', 'action')
+description.append('tspan')
+    .text(' by choosing the year')
 description.append('tspan')
     .text('scroll')
     .attr('dy', 30)
@@ -233,68 +230,9 @@ description.append('tspan')
 description.append('tspan')
     .text(' to explore data')
 
+
 sourceInfo = svg.append('text')
   .text('Data source: patentsview.org (2018)')
   .attr('x', 15)
   .attr('y', height-20)
   .attr('id', 'source')
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-// var pointMarkers = svg.append( "g" );
-
-// pointMarkers.selectAll( "path" )
-//   .data( rodents_json.features )
-//   .enter()
-//   .append( "path" )
-//   .attr( "fill", "#900" )
-//   .attr( "stroke", "#999" )
-//   .attr( "d", geoPath );
-
-// function zoomed({transform}) {
-//   console.log(transform)
-//   map.attr("transform", transform);
-//   map.selectAll("circle")
-//     .attr("r", markerRadius/transform.k); // keep marker size constant on screen
-// }
-
-
-// function zoomed() {
-//   projection.translate(d3.event.translate).scale(d3.event.scale); // modify the projection
-//   g.selectAll("path").attr("d", path);   // update the paths
-//
-//   // update the circles/points:
-//   svg.selectAll("circle")
-//     .attr("cx", function (d) { return projection(d)[0]; })
-//     .attr("cy", function (d) { return projection(d)[1]; });
-// }
-
-
-  // function main() {
-  //   var margin = { top:50, left: 50, right: 50, bottom: 50},
-  //     height = 400 - margin.top - margin.bottom,
-  //     width = 800 - margin.left - margin.right;
-  //
-  //   var svg = d3.select("#map")
-  //         .append("svg")
-  //         .attr("height", height + margin.top + margin.bottom)
-  //         .attr("width", width + margin.left + margin.right)
-  //         .append("g")
-  //         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  //
-  //   d3.json("world.topojson", function(data) {
-  //     console.log(data);
-  //   });
-  //
-  //   d3.queue()
-  //     .defer(d3.json, "world.topojson")
-  //     .await(ready);
-  //
-  //   function ready (error, data) {
-  //     // console.log(data)
-  //
-  //   }
-  //
-  // }
-  //
-  // main();
